a1318654589cce5b972a83205b6aafbd8049d967
Falren <nokuchie@gmail.com>
2020-11-17 09:31:47 +0200

    devise reg && login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   enum status: %i[teacher student]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :status, presence: true
 end
